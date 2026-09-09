0535a11621a5718029cb78f0eeca335f8fc3f823
StevenLhmn <steven.lehmann@haw-hamburg.de>
2026-09-09 11:45:13 +0200

    finish abstract grouping algo class

--- src/grouping/grouping_algo.py
+++ src/grouping/grouping_algo.py
@@ -1,19 +1,21 @@
 from abc import ABC, abstractmethod
 from src.data.grouped_region import GroupedRegion
+from src.data.region import Region
+from src.animation.trajectory import Trajectory
 
 
 class GroupingAlgo(ABC):
     @abstractmethod
-    def group(self) -> GroupedRegion:
+    def group(self, region: Region) -> GroupedRegion:
         """Group the regions."""
         pass
 
     @abstractmethod
-    def get_trajectory(self) -> None:
+    def get_trajectory(self) -> Trajectory:
         """Get the animation data in form of trajectory object."""
         pass
 
     @abstractmethod
     def __str__(self) -> str:
         """Return a readable algorithm name."""
-        pass
+        return
